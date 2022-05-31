c7af36b3240b5f1161740a4b472272b83fe1a9ae
Sanduni Kaushalya <pathiraja9897@gmail.com>
2022-05-31 12:01:23 +0530

    vi editor test Added .gitignore and removed bin, .classpath from staging

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world sanduni!");
+        System.out.println("Hello world sanduni Kaushalya!");
     }
 }
